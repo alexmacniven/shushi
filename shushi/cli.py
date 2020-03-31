28f453a71dd9e1865e459398b62967149a120968
Alex Macniven <macniven.ap@gmail.com>
2020-03-31 15:14:40 +0100

    Adds context to cli command and colours

--- shushi/cli.py
+++ shushi/cli.py
@@ -1,4 +1,5 @@
 import click
+import os
 from . import core
 from . import crypto
 from typing import List
@@ -7,13 +8,28 @@ from .constants import APPDATA
 
 
 @click.group()
-def cli():
-    pass
+@click.option("-p", "--password", default=None, help="Vault password")
+@click.pass_context
+def cli(ctx, password):
+    ctx.ensure_object(dict)
+    password_ = os.environ.get("SHUSHI_PASSWORD", password)
+    if password_ is not None:
+        ctx.obj["password"] = password_
+        ctx.obj["salt"] = core.fetch_salt(APPDATA)
+        ctx.obj["vault"] = core.fetch_vault(APPDATA)
+        ctx.obj["decrypted"] = crypto.decrypt(
+            ctx.obj.get("salt"),
+            ctx.obj.get("password"),
+            ctx.obj.get("vault")
+        )
+    else:
+        click.secho("A password has not been supplied", fg="bright_red")
+        ctx.abort()
 
 
 @cli.command(help="Creates a new vault")
-@click.argument("password")
 @click.option(
+    "-f",
     "--force",
     is_flag=True,
     flag_value=True,
@@ -23,13 +39,12 @@ def make(password, force):
     vault_path = APPDATA.joinpath("vault")
     if not vault_path.is_file() or force:
         core.make_vault(APPDATA, password)
-        click.echo("A new vault has been created\n")
+        click.secho("A new vault has been created", fg="green")
     else:
-        click.echo("Vault already exists. Run with --force to override\n")
+        click.secho("A vault already exists", fg="red")
 
 
 @cli.command(help="Adds a new item")
-@click.argument("password")
 @click.argument("name")
 @click.option(
     "--force",
@@ -37,17 +52,19 @@ def make(password, force):
     flag_value=True,
     help="Overwrites if an item exists with the same name"
 )
-def add(password, name, force):
-    salt: bytes = core.fetch_salt(APPDATA)
-    vault: bytes = core.fetch_vault(APPDATA)
-    decrypted: dict = crypto.decrypt(salt, password, vault)
+@click.pass_context
+def add(ctx, name, force):
     new_item: dict = item_builder(name)
-    if core.add_item(new_item, decrypted, force):
-        click.echo(f"Added new item: {name}")
-        encrypted: bytes = crypto.encrypt(salt, password, decrypted)
+    if core.add_item(new_item, ctx.obj.get("decrypted"), force):
+        click.secho(f"Added new item: {name}", fg="green")
+        encrypted: bytes = crypto.encrypt(
+            ctx.obj.get("salt"),
+            ctx.obj.get("password"),
+            ctx.obj.get("decrypted")
+        )
         core.dump_vault(APPDATA, encrypted)
     else:
-        click.echo("Item already exists. Run with --force to override\n\n")
+        click.secho("Item already exists", fg="red")
 
 
 def item_builder(name: str) -> dict:
@@ -64,41 +81,37 @@ def item_builder(name: str) -> dict:
 
 
 @cli.command(help="Returns an item")
-@click.argument("password")
 @click.argument("name")
-def get(password, name):
-    salt: bytes = core.fetch_salt(APPDATA)
-    vault: bytes = core.fetch_vault(APPDATA)
-    decrypted: dict = crypto.decrypt(salt, password, vault)
-    item: VaultRecord = core.get_item(name, decrypted)
+@click.pass_context
+def get(ctx, name):
+    item: VaultRecord = core.get_item(name, ctx.obj.get("decrypted"))
     if item is not None:
         for key, value in item.__dict__.items():
-            click.echo(f"{key} -> {value}")
+            click.secho(f"{key} -> ", nl=False, fg="yellow")
+            click.echo(f"{value}")
     else:
-        click.echo(f"Item not matched: {name}")
+        click.secho(f"Item not matched: {name}", fg="red")
 
 
 @cli.command(help="Removes an item")
-@click.argument("password")
 @click.argument("name")
-def remove(password, name):
-    salt: bytes = core.fetch_salt(APPDATA)
-    vault: bytes = core.fetch_vault(APPDATA)
-    decrypted: dict = crypto.decrypt(salt, password, vault)
-    if core.remove_item(name, decrypted):
+@click.pass_context
+def remove(ctx, name):
+    if core.remove_item(name, ctx.obj.get("decrypted")):
         click.echo(f"Item has been removed: {name}")
-        encrypted: bytes = crypto.encrypt(salt, password, decrypted)
+        encrypted: bytes = crypto.encrypt(
+            ctx.obj.get("salt"),
+            ctx.obj.get("password"),
+            ctx.obj.get("decrypted")
+        )
         core.dump_vault(APPDATA, encrypted)
     else:
-        click.echo(f"Item not matched: {name}")
+        click.secho(f"Item not matched: {name}", fg="red")
 
 
 @cli.command(help="Returns all item names")
-@click.argument("password")
-def list(password):
-    salt: bytes = core.fetch_salt(APPDATA)
-    vault: bytes = core.fetch_vault(APPDATA)
-    decrypted: dict = crypto.decrypt(salt, password, vault)
-    items: List = core.list_items(decrypted)
+@click.pass_context
+def list(ctx):
+    items: List = core.list_items(ctx.obj.get("decrypted"))
     for item in items:
-        click.echo(item)
+        click.secho(item, fg="yellow")
